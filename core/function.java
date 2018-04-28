00468162e2cb3882037c42a8f4bbb47cda4c3f49
Kashi Nath Datta <kashi.re2014@gmail.com>
2018-04-28 10:09:50 +0530

    Asynchronous non-adaptive mode CDS(p3) protocol where wakeup = 5 and total slot = 21

--- core/function.java
+++ core/function.java
@@ -1,133 +1,14 @@
 package core;
 
 public class function {
+
 		public boolean function(Double check ) {
 			
-				if(check==1||check==2||check==3||check==4||check==5||check==9||check==13){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function1(Double check ) {
-			
-				if(check==1||check==2||check==3||check==4||check==6||check==10||check==14){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function2(Double check ) {
-			
-				if(check==1||check==2||check==3||check==4||check==7||check==11||check==15){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function3(Double check ) {
-			
-				if(check==1||check==2||check==3||check==4||check==8||check==12||check==16){
+				if(check==1||check==2||check==5||check==15||check==17){
 					return true;
 				}
 			
 			return false;
 		}
 		
-		public boolean function4(Double check ) {
-			
-				if(check==5||check==6||check==7||check==8||check==3||check==11||check==15){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function5(Double check ) {
-			
-				if(check==5||check==6||check==7||check==8||check==2||check==10||check==14){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function6(Double check ) {
-			
-				if(check==5||check==6||check==7||check==8||check==1||check==9||check==13){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function7(Double check ) {
-			
-				if(check==5||check==6||check==7||check==8||check==4||check==12||check==16){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function8(Double check ) {
-			
-				if(check==13||check==14||check==15||check==16||check==4||check==8||check==12){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function9(Double check ) {
-			
-				if(check==13||check==14||check==15||check==16||check==3||check==7||check==11){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function10(Double check ) {
-			
-				if(check==13||check==14||check==15||check==16||check==2||check==6||check==10){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function11(Double check ) {
-			
-				if(check==13||check==14||check==15||check==16||check==1||check==5||check==9){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function12(Double check ) {
-			
-				if(check==9||check==10||check==11||check==12||check==4||check==8||check==16){
-					return true;
-				}
-			
-			return false;
-		}
-		public boolean function13(Double check ) {
-			
-				if(check==9||check==10||check==11||check==12||check==3||check==7||check==15){
-					return true;
-				}
-			
-			return false;
-		}	
-		public boolean function14(Double check ) {
-			
-				if(check==9||check==10||check==11||check==12||check==2||check==6||check==14){
-					return true;
-				}
-			
-			return false;
-		}	
-		public boolean function15(Double check ) {
-			
-				if(check==9||check==10||check==11||check==12||check==1||check==5||check==13){
-					return true;
-				}
-			
-			return false;
-		}			
 }
